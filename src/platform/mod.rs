31251b7b970c6c8ec8d15b8c8ef7df779b674c38
Youjie Zheng <Azure_stars@126.com>
2024-11-10 17:41:29 +0800

    [chore] Not import aarch64 at dummy

--- src/platform/mod.rs
+++ src/platform/mod.rs
@@ -1,13 +1,6 @@
 //! Platform-specific operations.
 
 cfg_if::cfg_if! {
-    if #[cfg(target_arch = "aarch64")]{
-        mod aarch64_common;
-        pub use self::aarch64_common::*;
-    }
-}
-
-cfg_if::cfg_if! {
     if #[cfg(all(target_arch = "x86_64", platform_family = "x86-pc"))] {
         mod x86_pc;
         pub use self::x86_pc::*;
@@ -17,18 +10,28 @@ cfg_if::cfg_if! {
     } else if #[cfg(all(target_arch = "aarch64", platform_family = "aarch64-qemu-virt"))] {
         mod aarch64_qemu_virt;
         pub use self::aarch64_qemu_virt::*;
+        mod aarch64_common;
+        pub use self::aarch64_common::*;
     } else if #[cfg(all(target_arch = "aarch64", platform_family = "aarch64-raspi"))] {
         mod aarch64_raspi;
         pub use self::aarch64_raspi::*;
+        mod aarch64_common;
+        pub use self::aarch64_common::*;
     } else if #[cfg(all(target_arch = "aarch64", platform_family = "aarch64-bsta1000b"))] {
         mod aarch64_bsta1000b;
         pub use self::aarch64_bsta1000b::*;
+        mod aarch64_common;
+        pub use self::aarch64_common::*;
     } else if #[cfg(all(target_arch = "aarch64", platform_family = "aarch64-phytiumpi"))] {
         mod aarch64_phytiumpi;
         pub use self::aarch64_phytiumpi::*;
+        mod aarch64_common;
+        pub use self::aarch64_common::*;
     } else if #[cfg(all(target_arch = "aarch64", platform_family = "aarch64-rk3588j"))] {
         mod aarch64_rk3588j;
         pub use self::aarch64_rk3588j::*;
+        mod aarch64_common;
+        pub use self::aarch64_common::*;
     } else {
         mod dummy;
         pub use self::dummy::*;
